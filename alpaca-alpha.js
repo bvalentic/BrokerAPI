3b0750add28144be73c17427ca8f0825a5a1871b
bvalentic <rvalentic@outlook.com>
2026-03-29 13:01:55 -0400

    Add NodeJS files, connect to Alpaca account.

--- alpaca-alpha.js
+++ alpaca-alpha.js
@@ -1,8 +1,8 @@
 const Alpaca = require('@alpacahq/alpaca-trade-api')
 
 const alpaca = new Alpaca({
-  keyId: 'PKRL23VGUL3SAPQDDGXVD5CXBC',
-  secretKey: 'EwVaE1y6JM7pR7wZDHm76WiDsjXfxQnng1xjG9cHjpyr',
+  keyId: 'KEY',
+  secretKey: 'SECRET',
   paper: true,
 })
 
